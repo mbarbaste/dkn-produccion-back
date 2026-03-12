67fb757e6372a9ce6d9a1becc92c8f2496ef3d81
Martín Barbaste <30574843+mbarbaste@users.noreply.github.com>
2026-03-12 19:36:57 -0300

    Update db-test.php

--- db-test.php
+++ db-test.php
@@ -3,7 +3,7 @@
         // Properties
         private $dbhost = 'localhost';
         private $dbuser = 'root';
-        private $dbpass = 'dr0p3smb';
+        private $dbpass = 'xXxXxX';
 
         // Test DB
         private $dbname = 'dolkin-prod-testing';
